44fe8626ed83125394cf89cbb13240b4fe01543b
Kesha Patel <keshapatel2699@gmail.com>
2020-10-02 12:06:42 +0300

    add variables

---
+++ js/script.js
@@ -0,0 +1,4 @@
+const century, year, month, dayOfMonth, dayOfWeek, day;
+const dayOfWeek = ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"];
+const maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku","Yaw", "Kofi", "Kwame"];
+const femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
