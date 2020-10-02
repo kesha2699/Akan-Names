120e87fd6f840f47948d25a5919bc3b31d779ea3
Kesha Patel <keshapatel2699@gmail.com>
2020-10-02 21:22:36 +0300

    add js functions

--- js/script.js
+++ js/script.js
@@ -1,4 +1,58 @@
-const century, year, month, dayOfMonth, dayOfWeek, day;
-const dayOfWeek = ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"];
-const maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku","Yaw", "Kofi", "Kwame"];
-const femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+
+//var dayOfWeek = ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"];//
+
+function getAkanName() {
+    var year =document.getElementById("year").value;
+    var month =document.getElementById("month").value;
+    var dayOfMonth =document.getElementById("monthday").value;
+    var maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku","Yaw", "Kofi", "Kwame"];
+    var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+    
+
+    //var maleNames = document.getElementById("male");//
+   //var femaleNames = document.getElementById("female");//
+
+    var day=new Date();
+    day.setDate(parseInt(dayOfMonth));
+    day.setMonth(parseInt(month)-1);
+    day.setFullYear(parseInt(year));
+    var Day= day.getDay();
+    
+    if (Day === 0){
+        alert("sunday");
+    }else if(Day === 1){
+        alert("monday");
+    }else if(Day === 2){
+        alert("tuesday");
+    }else if(Day === 3){
+        alert("wednesday");
+    }else if(Day === 4){
+        alert("thursday");
+    }else if(Day === 5){
+        alert("friday");
+    }else if(Day === 6){
+        alert("saturday");
+    }
+
+    if(dayOfMonth == "" || dayOfMonth >31){
+        alert("Enter valid day");
+    }else if(month == "" || month >12){
+        alert("Enter valid month");
+    
+    }else if(year == "" || year >2030){
+        alert("Enter valid year");
+    
+    }else if(document.getElementById("male").checked == true && document.getElementById("female").checked == false){
+        alert("Your Akan name is " + maleNames[Day]);
+    
+    }else if(document.getElementById("female").checked == true && document.getElementById("male").checked == false){
+        alert("Your Akan name is " + femaleNames[Day]);
+    
+    }
+}
+
+    
+
+
+
+   
